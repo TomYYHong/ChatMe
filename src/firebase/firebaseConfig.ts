5d86e2f715ef36e638744edfa8156dea99a67fd3
Tom Hong <tom.y.y.hong@gmail.com>
2023-11-02 11:13:59 -0400

    set the api key to env var

--- src/firebase/firebaseConfig.ts
+++ src/firebase/firebaseConfig.ts
@@ -4,11 +4,11 @@ import { getAuth } from 'firebase/auth';
 import 'firebase/firestore';
 //  const apiKey1 = import.meta.env.REACT_APP_FIREBASE_API_KEY;
 // const apiKey2 = process.env.REACT_APP_FIREBASE_API_KEY;
- console.log("apiKey: ",import.meta.env.VITE_REACT_APP_FIREBASE_API_KEY);
+//  console.log("apiKey: ",import.meta.env.VITE_REACT_APP_FIREBASE_API_KEY);
 const firebaseConfig = {
     //  apiKey: apiKey1,
     // apiKey: process.env.FIREBASE_API_KEY,
-    apiKey:"AIzaSyAHajRA9tkkbncJDfrWwS81hRxDNsHBNT8",
+    apiKey:import.meta.env.VITE_REACT_APP_FIREBASE_API_KEY,
     authDomain: "chatme-af595.firebaseapp.com",
     projectId: "chatme-af595",
     storageBucket: "chatme-af595.appspot.com",
