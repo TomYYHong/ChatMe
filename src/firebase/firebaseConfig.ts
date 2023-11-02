fbaa824998b7f1ccda29061d2aafcb3834fb9594
Tom Hong <tom.y.y.hong@gmail.com>
2023-11-01 22:46:06 -0400

    test api key

--- src/firebase/firebaseConfig.ts
+++ src/firebase/firebaseConfig.ts
@@ -2,10 +2,11 @@ import { initializeApp } from 'firebase/app';
 import 'firebase/auth';
 import { getAuth } from 'firebase/auth';
 import 'firebase/firestore';
-// const apiKey = import.meta.env.FIREBASE_API_KEY;
-const apiKey = process.env.FIREBASE_API_KEY;
+ const apiKey1 = import.meta.env.REACT_APP_FIREBASE_API_KEY;
+const apiKey2 = process.env.REACT_APP_FIREBASE_API_KEY;
+console.log("apiKey1: ",apiKey1,"apikey2: ",apiKey2);
 const firebaseConfig = {
-     apiKey: apiKey,
+     apiKey: apiKey1,
     // apiKey: process.env.FIREBASE_API_KEY,
     authDomain: "chatme-af595.firebaseapp.com",
     projectId: "chatme-af595",
@@ -14,8 +15,6 @@ const firebaseConfig = {
     appId: "1:759633202:web:38b8939eba8198adee332b",
     measurementId: "G-DWNJW5C41H"
   };
-
-  console.log("apiKey: ",apiKey);
 
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
