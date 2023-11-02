ab61e82499daf2d4f4b9da1c8caa36fdd10f229f
Tom Hong <tom.y.y.hong@gmail.com>
2023-11-01 22:33:02 -0400

    try api key

--- src/firebase/firebaseConfig.ts
+++ src/firebase/firebaseConfig.ts
@@ -2,9 +2,9 @@ import { initializeApp } from 'firebase/app';
 import 'firebase/auth';
 import { getAuth } from 'firebase/auth';
 import 'firebase/firestore';
-
+const apiKey = import.meta.env.FIREBASE_API_KEY;
 const firebaseConfig = {
-     apiKey: import.meta.env.FIREBASE_API_KEY,
+     apiKey: apiKey,
     // apiKey: process.env.FIREBASE_API_KEY,
     authDomain: "chatme-af595.firebaseapp.com",
     projectId: "chatme-af595",
@@ -13,6 +13,8 @@ const firebaseConfig = {
     appId: "1:759633202:web:38b8939eba8198adee332b",
     measurementId: "G-DWNJW5C41H"
   };
+
+  console.log("apiKey: ",apiKey);
 
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
